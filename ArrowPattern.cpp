8b89609b557ce55d791a010a36c0ae745d72ee5b
Susmita-Dey <79099734+Susmita-Dey@users.noreply.github.com>
2021-12-01 18:53:50 +0530

    Create ArrowPattern.cpp

---
+++ ArrowPattern.cpp
@@ -0,0 +1,57 @@
+/* Printing Arrow Pattern Using Stars */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, n1, n2, spaces, j;
+    cout << "Enter a number: ";
+    cin >> n;
+    i = 1;
+    n1 = (n + 1) / 2;
+    n2 = n / 2;
+
+    // first part
+    while (i <= n1)
+    {
+        spaces = 1;
+        // spaces
+        while (spaces <= i - 1)
+        {
+            cout << " ";
+            spaces = spaces + 1;
+        }
+        j = 1;
+        // increasing sequence
+        while (j <= i)
+        {
+            cout << "* ";
+            j = j + 1;
+        }
+        cout << "\n";
+        i = i + 1;
+    }
+
+    // second part
+    i = n2;
+    while (i >= 1)
+    {
+        spaces = 1;
+        // spaces
+        while (spaces <= i - 1)
+        {
+            cout << " ";
+            spaces = spaces + 1;
+        }
+        j = 1;
+        // decreasing sequence
+        while (j <= i)
+        {
+            cout << "* ";
+            j = j + 1;
+        }
+        cout << "\n";
+        i = i - 1;
+    }
+}
